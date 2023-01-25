3512ae83c1719fe87193ec7a16060d183e060d52
Nezamov.Andrei <lynch.0945@gmail.com>
2023-01-25 16:54:51 +0900

    added example005 if else

---
+++ example005_if_else/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+if(username == "Андрей")
+{
+    Console.WriteLine("Ура! Это же Андей!");
+}
+else
+{
+    Console.WriteLine("Добрый день");
+}
